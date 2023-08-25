498ac4460d5e9be97bb501c21c869101e3d042ad
StefanoMela <stefano.mela25@gmail.com>
2023-08-25 16:23:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const images = [
+    {
+      image: 'img/01.webp',
+      title: "Marvel's Spiderman Miles Morale",
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    },
+    {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    },
+    {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+    },
+    {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    },
+    {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+    },
+];
+
+
+// RECUPERO ELEMENTI HTML UTILI
+const imgContainer = document.getElementById("slides-container");
+const previousArrow = document.getElementById("go-prev");
+const nextArrow = document.getElementById("go-next");
+let activeImg = 0;
+
+for (let i in images) {
+
+  let image = images[i];
+  
+  imgContainer.innerHTML += 
+  `
+  <img src="./${image.image}" class="img" alt="${image.text}">
+  `
+};
+
+
+console.log(document.getElementsByClassName("img")[activeImg].classList.add("active-img"));
